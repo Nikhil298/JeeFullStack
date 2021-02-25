cc005fed07ea4b809f52b77cca1211a8beb1f0be
HP <HP@LAPTOP-OCLISKAK>
2021-02-25 22:02:29 +0530

    updated to hello

--- Test.java
+++ Test.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class HelloJava {
 	public static void main(String[] args) {
 		 Date d=new Date(); 
-	     System.out.println(d);  	
+	     System.out.println(d);  
+	     System.out.println("hello");
 	}
 }
